d7ef7be1195df62298af6c00283fc62109e6435e
samwel <samwuelkinuthia@gmail.com>
2017-12-14 14:36:27 +0300

    comments removed

--- src/providers/weather/weather.ts
+++ src/providers/weather/weather.ts
@@ -1,12 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-/*
-  Generated class for the WeatherProvider provider.
-
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class WeatherProvider {
 
