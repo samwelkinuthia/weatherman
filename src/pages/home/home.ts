449bf6d7eee58221256414ec538cec5f812c99e8
samwel <samwuelkinuthia@gmail.com>
2017-12-14 17:26:52 +0300

    import weather provider service

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { WeatherProvider } from '../../providers/weather/weather';
+
 
 @Component({
   selector: 'page-home',
@@ -7,7 +9,7 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private weatherProvider: WeatherProvider) {
 
   }
 
