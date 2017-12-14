f2b2877d0f33eee8a077327f3821dd6d8937b892
samwel <samwuelkinuthia@gmail.com>
2017-12-14 18:04:44 +0300

    remove deprecated map function for httpclient

--- src/providers/weather/weather.ts
+++ src/providers/weather/weather.ts
@@ -1,19 +1,18 @@
 import { Injectable } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import 'rxjs/add/operator/map';
+import { HttpClient } from '@angular/common/http';
 
 
 @Injectable()
 export class WeatherProvider {
   apiKey = 'c2f1c3b262b367ca';
   url;
-  constructor(public http: HttpClientModule) {
+  constructor(public http: HttpClient) {
     console.log('Hello WeatherProvider Provider');
     this.url='http://api.wunderground.com/api/'+this.apiKey+'/conditions/q/Kenya';
   }
 
-  getWeather(city){
-    return this.http.get(this.url+'/'+city+'.json')
-      .map(res => res.json());
+
+  getWeather(town){
+    return this.http.get(this.url+'/'+town+'.json')
   }
 }
