cdd4898c379fe06719b7fc3400a5db52ed3cdd67
samwel <samwuelkinuthia@gmail.com>
2017-12-14 17:27:04 +0300

    import http and map function

--- src/providers/weather/weather.ts
+++ src/providers/weather/weather.ts
@@ -1,11 +1,13 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
 
 @Injectable()
 export class WeatherProvider {
   apiKey = 'c2f1c3b262b367ca';
   url;
-  constructor(public http: HttpClient) {
+  constructor(public http: Http) {
     console.log('Hello WeatherProvider Provider');
     this.url='http://api.wunderground.com/api/'+this.apiKey+'/conditions/q/Kenya';
   }
