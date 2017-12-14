2005ef96f167355b3feaaaa225f56ca3f44fd442
samwel <samwuelkinuthia@gmail.com>
2017-12-14 19:22:12 +0300

    set town variable

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,21 +1,16 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the SettingsPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-settings',
   templateUrl: 'settings.html',
 })
 export class SettingsPage {
+  town: string;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.town = 'Nairobi';
   }
 
   ionViewDidLoad() {
