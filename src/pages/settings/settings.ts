2399a2b107f22b87dcc09639755fc4e0e2e6ce4c
samwel <samwuelkinuthia@gmail.com>
2017-12-14 19:33:04 +0300

    import storage

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
+
 
 @IonicPage()
 @Component({
@@ -9,7 +11,10 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class SettingsPage {
   town: string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private storage:Storage) {
     this.town = 'Nairobi';
   }
 
