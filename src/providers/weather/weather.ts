b7aa16a68f06ffba704d07c0469da1fa9bb2719a
samwel <samwuelkinuthia@gmail.com>
2017-12-14 15:18:19 +0300

    added apikey and base url

--- src/providers/weather/weather.ts
+++ src/providers/weather/weather.ts
@@ -3,9 +3,10 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class WeatherProvider {
-
+  apiKey = 'c2f1c3b262b367ca';
+  url;
   constructor(public http: HttpClient) {
     console.log('Hello WeatherProvider Provider');
+    this.url='http://api.wunderground.com/api/'+this.apiKey+'/conditions/q';
   }
-
 }
