b4c31a2997c593b7c05088ba6a92c636c1fbb75e
samwel <samwuelkinuthia@gmail.com>
2018-01-10 19:06:39 +0300

    fetched api key from environment provider

--- src/providers/weather/weather.ts
+++ src/providers/weather/weather.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
-
+import { EnvironmentProvider} from "./environment";
 
 @Injectable()
 export class WeatherProvider {
-  apiKey = 'c2f1c3b262b367ca';
+  apiKey: any;
   url;
-  constructor(public http: Http) {
-    console.log('Hello WeatherProvider Provider');
+  constructor(public http: Http, public environmentProvider: EnvironmentProvider ) {
+    this.apiKey = this.environmentProvider.provideKey();
     this.url='http://api.wunderground.com/api/'+this.apiKey+'/conditions/q/Kenya';
   }
 
